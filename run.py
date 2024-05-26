e9d6b1ee2463b119ec63e544ba8410d2701365a6
Fraser Sharp <fraser.sharp+github@pm.me>
2024-05-26 22:25:45 +0100

    Add logging, optional mirroring

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import ffmpeg
+from pathlib import Path
 
 
 # Get args
@@ -35,7 +36,7 @@ parser.add_argument(
     default=1080
 )
 parser.add_argument(
-    '-q', '--output-quality',
+    '-Q', '--output-quality',
     help='FFmpeg quality preset for output video.  See https://trac.ffmpeg.org/wiki/Encode/H.264#Preset',
     default='veryfast'
 )
@@ -44,7 +45,23 @@ parser.add_argument(
     help='Mirror video horizontally',
     default=False,
     action='store_true'
-) #TODO: actually implement this
+)
+parser.add_argument(
+    '-v', '--verbose',
+    help='Verbose FFmpeg logging',
+    default=False,
+    action='store_true'
+)
+parser.add_argument(
+    '-q', '--quiet',
+    help='Minimal logging output.  Output files will be overwritten automatically if they already exist.',
+    default=False,
+    action='store_true'
+)
+parser.add_argument(
+    '-o', '--output',
+    help='File to save output to.  Will default to \'overlayed-<ffmpeg preset>-<base video path>\'.'
+)
 
 #TODO: Output file path option
 args = parser.parse_args()
@@ -53,9 +70,12 @@ args = parser.parse_args()
 # Get video resolutions using ffmpeg,probe
 base_video_stream = next((stream for stream in ffmpeg.probe(args.base)['streams'] if stream['codec_type'] == 'video'), None)
 base_res = {'x': int(base_video_stream['width']), 'y': int(base_video_stream['height'])}
-
 overlay_video_stream = next((stream for stream in ffmpeg.probe(args.overlay)['streams'] if stream['codec_type'] == 'video'), None)
 overlay_res = {'x': int(overlay_video_stream['width']), 'y': int(overlay_video_stream['height'])}
+
+if not args.quiet:
+    print(f"Base video: \t'{args.base}' \t{base_res['x']}x{base_res['y']}")
+    print(f"Overlay video: \t'{args.overlay}' \t{overlay_res['x']}x{overlay_res['y']}")
 
 
 # Determine top-left corner position to place overlay
@@ -82,20 +102,46 @@ overlay_file = (
         .filter('scale', overlay_scaled_width, -1)
     )
 
-
 # Overlay the rear-view onto the base video
 overlay_filter = (
     ffmpeg.overlay(base_file, overlay_file, x=overlay_x_pos, y=overlay_y_pos)
         .filter('scale', args.output_resolution, -1)
     )
 
+if not args.quiet:
+    print(f"Output resolution: {args.output_resolution}x{int(args.output_resolution * (overlay_res['y'] / overlay_res['x']))}, quality preset: {args.output_quality}")
+
+# Apply mirror if requested
+if args.mirror:
+    if not args.quiet:
+        print('Output will be mirrored.')
+
+    overlay_filter = ffmpeg.hflip(overlay_filter)
+
+
+# Prepare transcode
+output_filename = args.output if args.output else 'overlayed-'+args.output_quality+'-'+args.base
+
+if not args.quiet:
+    print(f'Output will be saved to {output_filename}')
+
+if not args.quiet and Path(output_filename).is_file() and input('File already exists.  Overwrite it? (y/N) - ').lower() != 'y':
+    print('Not overwriting, bailing out.')  # We can't prompt in quiet mode, so overwrite
+    exit(1)
+
+command = ffmpeg.output(
+    overlay_filter,
+    base_file.audio, 
+    output_filename,
+    preset=args.output_quality
+).overwrite_output()
+
 
 # Run transcode
-command = ffmpeg.output(
-    overlay_filter, 
-    base_file.audio, 
-    'overlayed-'+args.output_quality+args.base,
-    preset=args.output_quality
-)
+if not args.quiet:
+    print('Running..')
 
-command.run(capture_stdout=False, capture_stderr=False)
+command.run(capture_stdout=not args.verbose, capture_stderr=not args.verbose)
+
+if not args.quiet:
+    print('Done!')
